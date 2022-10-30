c73a900caf0d49943c1f1325bd38e221134e2ec3
Konstantin Balashev <tripmannn@gmail.com>
2022-10-30 10:52:08 +0300

    added texture

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -6,6 +6,18 @@ export function many<T>(n: number, fn: (i: number) => T) {
     return [...Array(n | 0)].map((e, i) => fn(i));
 }
 
-export function rnd(x = 1): number {
-    return Math.random() * x;
+export function rnd(x = 1, y = 0): number {
+    return Math.random() * x + y;
+}
+
+export function rndi(x, y = 0): number {
+    return rnd(x, y) | 0;
+}
+
+export function rndb(x = 0.5): boolean {
+    return rnd() > x;
+}
+
+export function pick<T>(from:T[]):T {
+    return from[rndi(from.length)];
 }
--- src/example/example.tsx
+++ src/example/example.tsx
@@ -1,28 +1,27 @@
 import React from "react";
-import {Canvas, Img, ImgSrc, matrix, Rect, rnd} from "../core";
+import {Canvas, Img, ImgSrc, many, matrix, pick, Rect, rndi} from "../core";
 import {Texture} from "../core/Texture";
 
+const palette = ['#606c38', '#283618',
+    '#fefae0', '#dda15e', '#bc6c25'];
+
 export function Example() {
-    const props = {
-        width: 500,
-        height: 500
-    }
-    const s = 100
+    const props = {width: 500, height: 500};
+    const s = 100, count = 1;
     return <Canvas {...props} >
-
-        <ImgSrc src={'./test.svg'} id={'example-img'} />
-
-        <Texture id={'example-texture'} {...props}>
-            {matrix(3, 3, (x, y) => {
-                return <Rect x={x * s} y={y * s} width={s} height={s}
-                             fill={`hsl(${rnd(360)},55%,55%)`}  key={x+'-'+y}/>
-            })}
-        </Texture>
-
-        {matrix(5, 5, (x, y) => {
-            return <Img x={x * s} y={y * s} width={s} height={s}
-                        img={'example-texture'} key={x+'-'+y}/>
-        })}
+        <ImgSrc src={'./test.svg'} id={'example-img'}/>
+        {many(count, i => <Texture id={'example-texture' + i} {...props} key={i}>
+            {matrix(5, 5, (x, y) => <Rect
+                {...imgProps(x, y, s)} fill={pick(palette)}
+            />)}
+        </Texture>)}
+        {matrix(5, 5, (x, y) => <Img
+            {...imgProps(x, y, s)} img={'example-texture' + rndi(count)}
+        />)}
     </Canvas>
+}
+
+function imgProps(x, y, s) {
+    return {key: x + '_' + y, x: x * s, y: y * s, width: s, height: s,}
 }
 
